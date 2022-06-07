1dd580d681238e90d3a331c26661ff7f238638c1
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-06-07 21:41:11 +0300

    commit tree 0ca481f781a3a148c05a610ca99a41bfc926a4ba

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(game='roller-skate') {
+    return `This Saturday, I want to ${game}!`;
+}
+function mondayWork(response='go to the office') {
+    return `This Monday, I will ${response}.`;
+}
+function wrapAdjective(params="*") {
+    return function name(def="a hard worker") {
+        return `You are ${params}${def}${params}!`
+    }
+}
